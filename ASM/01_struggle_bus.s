b2976f75d56c88c0aa3c527659a92dddc6d3eeb6
Shandmo <fillerbarnesmail@gmail.com>
2024-03-01 00:22:11 -0500

    01_struggle_bus

---
+++ ASM/01_struggle_bus.s
@@ -0,0 +1,17 @@
+section .data
+msg db "Struggle Bus!", 0xA
+msg_len equ $-msg
+
+global _start
+
+section .text
+        _start:
+        mov edx, msg_len
+        mov ecx, msg
+        mov ebx, 1
+        mov eax, 4
+        int 0x80
+
+        mov eax, 1
+        int 0x80
+	;https://dev.to/tomassirio/hello-world-in-asm-x8664-jg7		
